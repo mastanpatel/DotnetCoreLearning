4abe626064db3b3c6cdb84ab40c913d14f2e1d49
mastan patel <m.mastanpatel@gmail.com>
2021-04-02 19:08:07 +0530

    added ef core

---
+++ EmployeeManagement/RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Tittle { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+        public Decimal Price { get; set; }
+
+    }
+}
